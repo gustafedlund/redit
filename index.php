d58f8742c2dab17055a8fea2bfe1e2be29e97842
gustafedlund <38979774+gustafedlund@users.noreply.github.com>
2018-11-03 20:52:14 +0100

    Fortsättning på post feed

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   include "init/config.php";
-
+  include "posts/print-posts.php";
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,8 +8,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/styles/posts.css">
-  <link rel="stylesheet" href="/styles/main.css">
+  <link rel="stylesheet" href="styles/posts.css">
+  <link rel="stylesheet" href="styles/main.css">
   <title>Redit</title>
 </head>
 <body>
@@ -17,7 +17,7 @@
 
   </div>
   <div class="post-container">
-
+    
   </div>
 
 </body>
--- posts/print-posts.php
+++ posts/print-posts.php
@@ -14,11 +14,16 @@ if (mysqli_num_rows($res) > 0) {
     $content = $rows['post_content'];
     $date = $rows['post_date'];
     $views = $rows['post_views'];
-    $like = $rows['post_likes'];
+    $likes = $rows['post_likes'];
     //Pushing HTML into the variable down here
     $printPosts .= "<a class = 'printed-post-frame'>";
-    $printPosts .= "<p class = 'printed-post-title'>$title</p>";
-    $printPosts .= "<p class = 'printed-post-content'>";
+      $printPosts .= "<p class = 'printed-post-title'>$title</p>";
+      $printPosts .= "<p class = 'printed-post-content'>$content</p>";
+      $printPosts .= "<p class = 'printed-post-creator'>$creator</p>";
+      $printPosts .= "<p class = 'printed-post-date'>$date</p>";
+      $printPosts .= "<p class = 'printed-post-views'>$views</p>";
+      $printPosts .= "<p class = 'printed-post-likes'>$likes</p>";
+    $printPosts .= "</a>";
 
 
   }
