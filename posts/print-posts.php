e4cd52218cd33a6a15f2c575e13d249699397353
gustafedlund <38979774+gustafedlund@users.noreply.github.com>
2018-11-03 20:35:41 +0100

    Funktionalitet med post-feed, m.m.
    Jag uppdaterade även namnet på php mappen till "init". Du kanske behöver ändra din kod som hänvisar till configen lite. SORRY ABOUT THAT

---
+++ posts/print-posts.php
@@ -0,0 +1,32 @@
+<?php
+include "../init/config.php";
+
+$sql = "SELECT * FROM posts ORDER BY post_date DESC";
+$res = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+$printPosts = ""; // We push html to be printed into this variable
+
+if (mysqli_num_rows($res) > 0) {
+  while ($rows = mysqli_fetch_assoc($res)) { // Fetch printable information from every row (post)
+    $id = $rows['post_id'];
+    $title = $rows['post_title'];
+    $creator = $rows['post_creator'];
+    $content = $rows['post_content'];
+    $date = $rows['post_date'];
+    $views = $rows['post_views'];
+    $like = $rows['post_likes'];
+    //Pushing HTML into the variable down here
+    $printPosts .= "<a class = 'printed-post-frame'>";
+    $printPosts .= "<p class = 'printed-post-title'>$title</p>";
+    $printPosts .= "<p class = 'printed-post-content'>";
+
+
+  }
+  echo $printPosts;
+} else {
+  // code...
+}
+
+
+
+?>
