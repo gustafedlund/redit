25903d3eedb48f49e483cc74786fa5a0236a48c8
Alice <alice.gothberg@me.com>
2018-11-09 12:45:39 +0100

    Update init/header.php

--- init/header.php
+++ init/header.php
@@ -6,13 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <link rel="stylesheet" href="../styles/posts.css">
-  <link rel="stylesheet" href="../styles/main.css">
-  <link rel="stylesheet" href="../styles/start.css">
+  <link rel="stylesheet" href="../styles/posts.css" type="text/css">
+  <link rel="stylesheet" href="../styles/main.css" type="text/css">
+  <link rel="stylesheet" href="../styles/start.css" type="text/css">
 
   <script type="text/javascript" src="../likeScript.js"></script>
 
   <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,800" rel="stylesheet">
   <title>Redit</title>
-  
+
 </head>
