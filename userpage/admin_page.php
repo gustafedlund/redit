ff8179f3679d8bd063991d9070eb3964e56c791d
Curryhero <38979782+Curryhero@users.noreply.github.com>
2018-11-09 11:18:12 +0100

    Print users

--- userpage/admin_page.php
+++ userpage/admin_page.php
@@ -4,13 +4,18 @@ require '../init/config.php';
 
 $query = "SELECT * FROM users";
 $result = mysqli_query($conn, $query);
-$row = mysqli_fetch_assoc($result);
 
-$username = $row['username'];
-$email = $row['email'];
-$post_permission = $row['post_permission'];
+if (mysqli_num_rows($result) >= 0) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    $user_id = $row['user_id'];
+    $username = $row['username'];
+    $email = $row['email'];
+    $post_permission = $row['post_permission'];
 
-echo "$username, $email, $post_permission";
+    echo "User ID: $user_id, Username: $username, Email: $email, Post permission: $post_permission";?><br><?php
+  }
+}
+
 
 
 ?>
