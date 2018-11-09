c8a47bc081489dfe38dcecf3f0709a48431f7fad
Curryhero <38979782+Curryhero@users.noreply.github.com>
2018-11-09 10:45:21 +0100

    retrieve user
    wwhat the title says pretty much

--- userpage/admin_page.php
+++ userpage/admin_page.php
@@ -1,9 +1,16 @@
 <?php
 
-$sql = "SELECT * FROM users";
+require '../init/config.php';
 
+$query = "SELECT * FROM users";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($result);
 
+$username = $row['username'];
+$email = $row['email'];
+$post_permission = $row['post_permission'];
 
+echo "$username, $email, $post_permission";
 
 
 ?>
