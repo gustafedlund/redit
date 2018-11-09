a630c4d91417e4518d9eeee74cbe70637af7e8de
gustafedlund <38979774+gustafedlund@users.noreply.github.com>
2018-11-09 12:51:12 +0100

    dadw

--- init/header.php
+++ init/header.php
@@ -6,9 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <link rel="stylesheet" href="../styles/posts.css" type="text/css">
-  <link rel="stylesheet" href="../styles/main.css" type="text/css">
-  <link rel="stylesheet" href="../styles/start.css" type="text/css">
+  <link rel="stylesheet" href="./styles/posts.css" type="text/css">
+  <link rel="stylesheet" href="./styles/main.css" type="text/css">
+  <link rel="stylesheet" href="./styles/start.css" type="text/css">
 
   <script type="text/javascript" src="../likeScript.js"></script>
 
