7a2c4965ea6bf7869cd5cceb458fb9db7af362e5
Curryhero <38979782+Curryhero@users.noreply.github.com>
2018-11-09 11:53:43 +0100

    Session lesson

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+  session_start();
   require "./init/config.php";
   include "posts/print-posts.php";
  ?>
--- init/config.php
+++ init/config.php
@@ -1,7 +1,5 @@
 <?php
 
-session_start();
-
 $db_host = '127.0.0.1';
 $db_username = 'root';
 $db_password = 'root';
