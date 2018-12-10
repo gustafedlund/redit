685f57260d872843dd92c0960256bb6e3afc2ff8
Alice <alice.gothberg@me.com>
2018-12-10 16:40:22 +0100

    VIEWS
    nu ser man hur många gånger inlägget öppnats :)

--- posts/show-post.php
+++ posts/show-post.php
@@ -4,6 +4,18 @@ session_start();
  include "../init/sidebar.php";
  require "../init/header.php";
  include "print-replies.php";
+
+ $post_id = $_GET['pid'];
+ $query = "SELECT post_views FROM posts WHERE post_id='$post_id'";
+ $result = mysqli_query($conn, $query);
+ if ($result) {
+   $data = $result->fetch_assoc();
+   $originalViews = $data['post_views'];
+   $updatedViews = $originalViews + 1;
+   $query2 = "UPDATE posts SET post_views='$updatedViews' WHERE post_id='$post_id'";
+   $result2 = mysqli_query($conn, $query2);
+ }
+
   ?>
 
   <div id="maincontent">
