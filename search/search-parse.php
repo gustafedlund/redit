057423a7b6e3b767fe2da311cf9b7c5473337dd5
gustafedlund <38979774+gustafedlund@users.noreply.github.com>
2019-01-30 12:06:27 +0100

    Update search-parse.php

--- search/search-parse.php
+++ search/search-parse.php
@@ -3,15 +3,29 @@ session_start();
 if ($_SESSION['loggedin'] !== TRUE) {
   header('Location: ../landingpage/login.php');
 }
-require "../init/config.php";
-require "../init/header.php";
-require "../init/sidebar.php";
+if (isset($_POST['submit-search'])) {
+  $kw = $_POST['search'];
+  require "../init/config.php";
+  require "../init/header.php";
+  require "../init/sidebar.php";
+
+  $sql = "SELECT * FROM posts";
+  $result = mysqli_query($conn, $sql);
+  $queryResults = mysqli_num_rows($results);
+
+  if ($queryResults > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+
+    }
+  }
 
 
-
-
-
-
-
-
+} else {
+  echo "Something went wrong, please try again.";
+}
  ?>
+
+ <h1>Sökresultat</h1>
+ <div class="search-results-wrapper">
+   <?php ?>
+ </div>
